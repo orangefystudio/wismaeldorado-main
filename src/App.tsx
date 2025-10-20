421d02aeb61f4cc8e41e823013dd69fc0e4151e1
orangefystudio <orangefystudio@gmail.com>
2025-10-20 22:10:55 +0800

    Fix React Router v7 future flags warning by enabling v7_startTransition and v7_relativeSplatPath

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,12 @@ const App = () => {
         <AdminAuthProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter
+            future={{
+              v7_startTransition: true,
+              v7_relativeSplatPath: true,
+            }}
+          >
             <AppContent />
           </BrowserRouter>
         </AdminAuthProvider>
